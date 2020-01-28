6e52363b869364a91411f3645ca37b7f1b382854
mscbr <scibor.maciej@gmail.com>
2020-01-28 14:14:36 +0100

    global style init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+
+import { createGlobalStyle } from 'styled-components';
 import './App.css';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 20px;
+    background: salmon;
+  }
+`;
 
 const App: React.FC = () => {
   return (
-    <div className='App'>
-      <h1>ST F U</h1>
-    </div>
+    <>
+      <GlobalStyle />
+      <div className='App'>
+        <h1>ST F U</h1>
+      </div>
+    </>
   );
 };
 
