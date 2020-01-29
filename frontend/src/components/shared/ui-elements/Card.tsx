948d946c2ce8c0cb1d04cf678965440e126e69b3
mscbr <scibor.maciej@gmail.com>
2020-01-29 14:59:29 +0100

    card w/ styles

--- frontend/src/components/shared/ui-elements/Card.tsx
+++ frontend/src/components/shared/ui-elements/Card.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import styled from "styled-components";
 
-const Card: React.FC = () => {
-    return <div>CARD</div>;
+import { secondary } from "../Styles";
+
+const StyledCard = styled.div`
+    margin: 32px auto;
+    padding: 16px 0;
+    width: 100%;
+    background: white;
+    border: 2px solid ${secondary};
+    border-radius: 12px;
+`;
+
+interface Props {
+    children?: JSX.Element[] | JSX.Element;
+}
+
+const Card: React.FC<Props> = props => {
+    return <StyledCard>{props.children}</StyledCard>;
 };
 
 export default Card;
