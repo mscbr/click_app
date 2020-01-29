7fe555fad659fc6a8793cdf2c27787c6fed621c9
mscbr <scibor.maciej@gmail.com>
2020-01-29 12:06:08 +0100

    global color palette

---
+++ frontend/src/components/shared/Styles.tsx
@@ -0,0 +1,7 @@
+// STYLE VARIABLES
+
+// color palette
+export const primary = "#f1fcfc";
+export const secondary = "#633a82";
+export const accentGray = "#72b5b7";
+export const accentTurq = "#40e0d0";
