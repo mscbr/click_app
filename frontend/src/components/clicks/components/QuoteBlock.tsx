262703ff779d20e93d74b37a6466aa2b9f42784a
mscbr <scibor.maciej@gmail.com>
2020-01-30 00:19:40 +0100

    quoteblock 0.1.0

--- frontend/src/components/clicks/components/QuoteBlock.tsx
+++ frontend/src/components/clicks/components/QuoteBlock.tsx
@@ -1,1 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
+
+// props:
+/// paragraph
+/// citee
+
+const StyledBlockQuote = styled.blockquote`
+    width: 100%;
+    padding: 1em 2em 1em 2em;
+`;
+
+interface Props {
+    paragraph?: string;
+    cite?: string;
+}
+
+const QuoteBlock: React.FC<Props> = props => {
+    const { paragraph, cite } = props;
+    return (
+        <StyledBlockQuote>
+            <p>{paragraph}</p>
+            <cite>
+                {`~ `}
+                {cite}
+            </cite>
+        </StyledBlockQuote>
+    );
+};
+
+export default QuoteBlock;
