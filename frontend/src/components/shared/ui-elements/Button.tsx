d2a0123ae200b6ad2cd9b40c6960fff73646bdf0
mscbr <scibor.maciej@gmail.com>
2020-01-30 13:22:58 +0100

    init

--- frontend/src/components/shared/ui-elements/Button.tsx
+++ frontend/src/components/shared/ui-elements/Button.tsx
@@ -1,1 +1,23 @@
-export default undefined;
+import React from 'react';
+import styled from 'styled-components';
+
+import { secondary } from '../Styles';
+
+const StyledButton = styled.button`
+    background: ${secondary};
+    color: white;
+    border: none;
+    border-radius: 6px;
+    margin-left: 16px;
+    text-transform: uppercase;
+`;
+
+interface Props {
+    text?: string;
+}
+
+const Button: React.FC<Props> = props => {
+    return <StyledButton>{props.text}</StyledButton>;
+};
+
+export default Button;
