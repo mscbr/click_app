0aad9505ba5e533ed62051b993c8443285458fdf
mscbr <scibor.maciej@gmail.com>
2020-01-30 18:38:31 +0100

    dom event stylings

--- frontend/src/components/shared/ui-elements/Button.tsx
+++ frontend/src/components/shared/ui-elements/Button.tsx
@@ -5,11 +5,24 @@ import { secondary } from '../Styles';
 
 const StyledButton = styled.button`
     background: ${secondary};
+    flex-grow: 1;
     color: white;
     border: none;
+    outline: none;
     border-radius: 6px;
     margin-left: 16px;
     text-transform: uppercase;
+    font-size: 21px;
+    transition: 0.2s;
+    &:hover {
+        cursor: pointer;
+        opacity: 0.9;
+    }
+    &:active {
+        cursor: pointer;
+        opacity: 0.7;
+        transform: scale(0.9);
+    }
 `;
 
 interface Props {
