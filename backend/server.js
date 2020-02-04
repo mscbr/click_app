e80c2d847d45c2eb5ceefc94a1033855791e7ee2
mscbr <scibor.maciej@gmail.com>
2020-02-04 17:54:09 +0100

    cleanup

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,22 @@
 // Importing Modules
-// const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-// importing files
-// const routes = require('./routes');
-
 // Define Global Variables
 const app = express();
 // const log = console.log;
-const PORT = process.env.PORT || 8080; // Step 1
-
-// Step 2
-// mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/my_database', {
-//     useNewUrlParser: true
-// });
+const PORT = process.env.PORT || 8080;
 
 // Configuration
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use('/', routes);
 
-// Step 3
-// if (process.env.NODE_ENV === 'production') {
 app.use(express.static('../frontend/build'));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/', 'build', 'index.html')); // relative path
 });
-// }
 
 app.listen(PORT, () => {
     console.log(`Server is starting at PORT: ${PORT}`);
