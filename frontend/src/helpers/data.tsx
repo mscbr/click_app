5d4b8ddc747ff86cc7b4efb0f454db1518c7c7c6
mscbr <scibor.maciej@gmail.com>
2020-02-09 18:47:41 +0100

    bandpass fix for little data

--- frontend/src/helpers/data.tsx
+++ frontend/src/helpers/data.tsx
@@ -10,8 +10,10 @@ export const scoreBoardBandPass = (
     team: string
 ) =>
     data.reduce((acc, item, i, src) => {
-        if (item.team === team) {
+        if (item.team === team && i - 3 && i + 4) {
             acc.push(...src.slice(i - 3, i + 4));
+        } else {
+            return src;
         }
         return acc;
     }, [] as { order: number; team: string; clicks: number }[]);
